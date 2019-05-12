9deba9a8eef5b4cd21c714a93839f4a9b80934a2
Mateusz <mrpeciak@gmail.com>
2019-05-12 15:22:03 +0200

    update player-board component, register Cloud Firestore

--- src/app/player-board/player-board.component.ts
+++ src/app/player-board/player-board.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, Input, EventEmitter } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Player } from './../player';
+import { Settings } from './../settings'
 
 @Component({
   selector: 'app-player-board',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlayerBoardComponent implements OnInit {
 
-  constructor() { }
+  @Input() playerName;
+  @Input() tableName;
+  
+  players: Player[] = [];
 
-  ngOnInit() {
+  settings: Settings[] = [];
+
+  constructor(public db: AngularFirestore) { 
+
   }
 
+  ngOnInit() {
+    this.db.collection(`${this.tableName}`).doc('players').collection('players').valueChanges().subscribe((players: Player[]) => this.players = players);
+    this.db.collection(`${this.tableName}`).valueChanges().subscribe((settings: Settings[]) => this.settings = settings);
+  }
+
+  test() {
+    console.log(
+      this.players
+    )
+    console.log(
+      this.settings
+    )
+  }
+
+
+  @Output() toggleWidgets = new EventEmitter<string>();
+
+
+  changeWidget(widget) {
+    this.toggleWidgets.emit(widget);
+  }
 }
+
