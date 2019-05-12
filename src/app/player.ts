9f72779a37e3f8a4ee17daf39516b55f21747151
Mateusz <mrpeciak@gmail.com>
2019-05-12 15:12:23 +0200

    add interfaces: player.ts and settings.ts

---
+++ src/app/player.ts
@@ -0,0 +1,5 @@
+export interface Player {
+     isActive: boolean;
+     playerCurrentTime: number;
+     playerName: string;
+}
---
+++ src/app/settings.ts
@@ -0,0 +1,6 @@
+export interface Settings {
+     admin: string;
+     numOfPlayers: number;
+     tableName: string;
+     currentNumOfPlayers: number;
+}
