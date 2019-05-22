cf3fb4bcaf421a8e822bbcc918941becdbf80b6c
Mateusz <mrpeciak@gmail.com>
2019-05-22 12:35:06 +0200

    update main-widget component

--- src/app/main-widget/main-widget.component.ts
+++ src/app/main-widget/main-widget.component.ts
@@ -3,7 +3,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-main-widget',
   template: `
-    <div class="main-container">
+    <div class="main-container main-widget">
+
       <p>
         TURN TIMER
       </p>
@@ -16,10 +17,21 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
         class="btn"
         (click)="newGame('join')"
       >DOŁĄCZ DO STOŁU</button>
+    
     </div>
 
   `,
-  styles: ['']
+  styles: [`
+    .main-widget {
+      justify-content: center;
+    }
+    p {
+      font-size: 3rem;
+      position: absolute;
+      top: 8rem;
+      font-weight: 300;
+    }
+  `]
 })
 export class MainWidgetComponent implements OnInit {
 
